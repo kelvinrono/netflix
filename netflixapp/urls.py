178454ca1c295264508d4837784ce9d6505e5155
cheruiyotkelvin <ronohkelvin99@gmail.com>
2021-07-22 15:14:35 +0300

    display the trailers

--- netflixapp/urls.py
+++ netflixapp/urls.py
@@ -3,18 +3,20 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include 
 from .views import Home,ProfileList,ProfileCreate,Watch,ShowMovie, ShowMovieDetail
 
 
 urlpatterns=[
  url(r'^mymovies', views.movies, name='myMovies'),
  url(r'^movie/(\d+)', views.single_movie, name = 'oneMovie'),
- url(r'^singlemovies/',views.single_movie, name = 'singles'),
 url('',Home.as_view()),
 url('profile/',ProfileList.as_view(),name='profile_list'),
 url('profile/create/',ProfileCreate.as_view(),name='profile_create'),
-url('watch/<str:profile_id>/',Watch.as_view(),name='watch')
+url('watch/<str:profile_id>/',Watch.as_view(),name='watch'),
+path('account/', include('django.contrib.auth.urls')),
+
+
 ]
 
 if settings.DEBUG:
